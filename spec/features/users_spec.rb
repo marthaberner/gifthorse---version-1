9a9691e9b9d77a2760f4760692c4920f96424a57
marthaberner <marthaberner.mb@gmail.com>
2014-05-09 17:28:45 -0600

    Updated testing to include specific error messages

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -17,7 +17,39 @@ feature 'User Accounts' do
       fill_in 'Password', with: 'meee'
       click_on 'Login'
     end
-
     expect(page).to have_content 'Hello'
+  end
+
+  scenario 'Username cannot be blank' do
+    visit '/'
+
+    within '.create_account' do
+      fill_in 'Username', with: ''
+      fill_in 'Password', with: '1234'
+      click_on 'Create Account'
+    end
+    expect(page).to have_content 'Username cannot be blank'
+  end
+
+  scenario 'Password cannot be blank' do
+    visit '/'
+
+    within '.create_account' do
+      fill_in 'Username', with: 'Hunter'
+      fill_in 'Password', with: ''
+      click_on 'Create Account'
+    end
+    expect(page).to have_content 'Password can\'t be blank'
+  end
+
+  scenario 'Password must be between min 4 characters ' do
+    visit '/'
+
+    within '.create_account' do
+      fill_in 'Username', with: 'Hunter'
+      fill_in 'Password', with: '12'
+      click_on 'Create Account'
+    end
+    expect(page).to have_content 'Password is too short (minimum is 4 characters)'
   end
 end
