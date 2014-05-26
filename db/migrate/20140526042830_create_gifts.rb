f60cefb89d9147d3f918ee568512defb60cfb585
marthaberner <marthaberner.mb@gmail.com>
2014-05-25 22:30:44 -0600

    Created Gifts table

--- db/migrate/20140526042830_create_gifts.rb
+++ db/migrate/20140526042830_create_gifts.rb
@@ -2,7 +2,7 @@ class CreateGifts < ActiveRecord::Migration
   def change
     create_table :gifts do |t|
       t.string :nickname
-      t.string :gift_url
+      t.string :url
     end
   end
 end
